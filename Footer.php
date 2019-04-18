4befa1d0d33f3999f6b80a7c6cbfa2f3fba89d3b
Ivan Khamar <ivan.khamar@gmail.com>
2019-04-18 20:02:55 +0300

    all rebooted

--- Footer.php
+++ Footer.php
@@ -1,5 +1,26 @@
 	<footer>
+		<style>
+		.footer {
+ 		position: fixed;
+  		left: 0;
+  		bottom: 0;
+  		width: 100%;
+  		background-color: red;
+  		color: white;
+  		text-align: center;
+}
+		</style>
+
+		<div class="footer" style="position: fixed;
+  		left: 0;
+  		bottom: 0;
+  		width: 100%;
+  		background-color: #151519;
+  		color: white;
+  		text-align: center;">
+  		<p>Footer</p>
 		<p>Copyrighted by three dudes. 2019</p>
+		</div>
 	</footer>
 </body>
 </html>
