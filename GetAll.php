b87707ee21dc155e2a8a7bbd351540f5667d0bd4
Svyatoslav Govda <svyatoslavgovda@gmail.com>
2019-04-18 19:49:15 +0300

    missunderstanding

--- GetAll.php
+++ GetAll.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	
-</body>
-</html>
-=======
 <?php include 'Header.php'; ?>
+
 <?php include 'Footer.php'; ?>
->>>>>>> 2141e76cbd8487c47b8494aa932a977648b888bf
