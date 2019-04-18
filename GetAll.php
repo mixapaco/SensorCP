4804cd99a27b2a1a556a8d858aac6e9b34dbb37e
Svyatoslav Govda <svyatoslavgovda@gmail.com>
2019-04-18 21:12:21 +0300

    connbd & showdata

--- GetAll.php
+++ GetAll.php
@@ -1,2 +1,31 @@
 <?php include 'Header.php'; ?>
+<?php 
+
+	require_once 'connection.php';
+	$query ="SELECT * FROM sensors";
+	$link = mysqli_connect($host, $user, $password, $database) 
+    or die("Ошибка " . mysqli_error($link)); 
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+if($result)
+{
+    $rows = mysqli_num_rows($result); 
+     
+    echo "<table><tr><th>Id запиту</th><th>Id Сенсора</th><th>К-сть</th><th>Дата</th></tr>";
+    for ($i = 0 ; $i < $rows ; ++$i)
+    {
+        $row = mysqli_fetch_row($result);
+        echo "<tr>";
+            for ($j = 0 ; $j < 4 ; ++$j) echo "<td>$row[$j]</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+     
+    
+    mysqli_free_result($result);
+}
+ 
+mysqli_close($link);
+?>
+     
+
 <?php include 'Footer.php'; ?>
---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php 
+$host = 'localhost';
+$database = 'db';
+$user = 'root';
+$password = '';
+?>
