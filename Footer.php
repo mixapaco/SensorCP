beaba2a1844803b71bff428281d11ddaf3670337
Sv9tcorejz <svyatoslavgovda@gmail.com>
2019-05-02 17:26:53 +0300

    bootstrap1

--- Footer.php
+++ Footer.php
@@ -1,8 +1,6 @@
-	<footer>
-		<div class="footerr"><hr>
-  			<p><span style="font-size: 33px;">&#9993:</span><span style="font-size: 33px; font-style: italic;"> woodborder@gmail.com</span></p>
-			<p>&#x24B8 Copyrighted by three dudes. 2019</p>
-		</div>
+	<footer id="footer" class="container-fluid">
+		<p>&#9993: woodborder@gmail.com</p>
+		<p>&#x24B8 Copyrighted by three dudes. 2019</p>
 	</footer>
 	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"></script>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
